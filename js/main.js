7c77cd754ee3e1cda5eb808d18980f327d409e6a
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-04 23:43:05 +0530

    2. jsonplaceholder.typicode ss

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// high order functions 
+/* A high order function is a function which does atleast one of these⬇️
+ ==>> Takes one or more functions as a argument (parameter)
+ ==>> Returns a function as the result */
+
