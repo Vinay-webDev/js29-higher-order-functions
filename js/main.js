3772d373876065a3f41ce7fec36f7465c3afcdb6
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-04 23:52:20 +0530

    3. forEach( ) method of array

--- js/main.js
+++ js/main.js
@@ -3,3 +3,26 @@
  ==>> Takes one or more functions as a argument (parameter)
  ==>> Returns a function as the result */
 
+import {userData} from './userData.js';
+
+userData.forEach( userData => {
+    console.log(userData);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
